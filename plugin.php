3a865b24b27f7575083447b7aeb7c682d0d62320
kyro46 <cjobst@wifa.uni-leipzig.de>
2017-11-10 11:56:00 +0100

    This adds some directives to the setup stated in the dev-guide.

--- plugin.php
+++ plugin.php
@@ -1,8 +1,8 @@
 <?php
 $id                = 'SignatureRFC3161';
-$version           = '0.0.8';
+$version           = '0.0.9';
 $ilias_min_version = '5.1.0';
-$ilias_max_version = '5.2.999';
+$ilias_max_version = '5.3.999';
 //09/2014:
 //$responsible = "Yves Annanias";
 //$responsible_mail = "yves.annanias@llz.uni-halle.de";
